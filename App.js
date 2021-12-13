a69f145a9038d9d818fc6b2399b94fda2ed05ea1
developer24 <developer24@www.elog.com>
2021-12-13 12:17:22 +0530

    Document Picker

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function App() {
+
+  const [fileUrl, setFileUrl] = useState();
+  const [fileSize, setFileSize] = useState();
+  const [fileName, setFileName] = useState();
+
+  const pickDocument = async () => {
+    let result = await DocumentPicker.getDocumentAsync({}).then(response => {
+      if (response.type === 'success') {
+        setFileUrl(response.uri)
+        setFileName(response.name)
+        setFileSize(response.size)
+      }
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FontAwesome name="folder-open" size={32} onPress={pickDocument} />
+      <Image source={{ uri: document }} style={{ width: 200, height: 200 }} />
     </View>
   );
 }
